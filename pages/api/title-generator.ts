225f025c4e004fb5b39d28c2921f3699b0453aa5
Meet8080 <patelarjun687@gmail.com>
2023-04-17 18:52:14 -0400

    added response for the api

--- pages/api/title-generator.ts
+++ pages/api/title-generator.ts
@@ -1,14 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
+import type { NextApiRequest, NextApiResponse } from "next";
+import { OpenAIApi } from "openai/dist/api";
+// import { configuration } from "../../utils/Configuration";
+import { configuration } from "@/utils/constant";
+import { error } from "console";
+// import { Configuration } from 'openai/dist/configuration';
 type Data = {
-  input: string
-}
-
-export default function handler(
+  input: string;
+};
+const openai = new OpenAIApi(configuration);
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const {input} = req.body
-  res.status(200).json({ name: input })
+  const { input } = req.body;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `give me 10 clickbait for ${input} \n\n`,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  const suggestion = response.data?.choices?.[0].text;
+
+  if (suggestion === undefined) throw new Error("Suggestion Not Found");
+  res.status(200).json({ result: suggestion });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,43 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [input, setInput] = useState("");
+  const [error, setError] = useState(false);
+  const [suggestion, setSuggestion] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (input.length < 30) setError(false);
+  }, [input]);
 
   // api request
 
   const submit = async () => {
-    const res = await fetch("/api/title-generator", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ input }),
-    });
-    const data = res.json();
-    console.log(data);
+    if (input.length > 30) return setError(true);
+    setLoading(true);
+
+    try {
+      const res = await fetch("/api/title-generator", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ input }),
+      });
+      const suggestion: { result: string } = await res.json();
+      const { result } = suggestion;
+      // console.log(data);
+      setSuggestion(result);
+    } catch (error) {
+      // console.log(error);
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <div className="">
@@ -30,13 +48,18 @@ export default function Home() {
           content="Anyone can generate title for their projects"
         />
       </Head>
-      <div className="max-w-7xl mx-auto py-12">
+      <div className="max-w-8xl mx-auto py-12">
         <h2 className="text-2xl font-bold text-center pb-2">
           Unique Title Generator
         </h2>
 
         <div className="flex justify-center gap-4 flex-col w-1/3 mx-auto">
           <div className="relative">
+            {error && (
+              <p className="pt-1 pb-1 text-red-600 text-sm">
+                Characters limit exceeded
+              </p>
+            )}
             <textarea
               rows={3}
               onChange={(e) => setInput(e.target.value)}
@@ -45,7 +68,11 @@ export default function Home() {
               value={input}
             />
             {/* Character Limit in bottom right of textarea */}
-            <div className="absolute bottom-4 right-3 text-xs text-gray-400">
+            <div
+              className={`absolute bottom-4 right-3 text-xs ${
+                error ? "text-red-500" : "text-gray-400"
+              } `}
+            >
               <span>{input.length}</span>/30
             </div>
           </div>
@@ -54,8 +81,22 @@ export default function Home() {
             className="bg-blue-500 hover:bg-blue-700
         text-white font-bold py-2 px-4 rounded"
           >
-            Generate
+            {loading ? "..loading" : "Generate"}
           </button>
+
+          {suggestion && (
+            <div className="mt-8">
+              <h2 className="text-lg pb-2">
+                This could be the name of your project:
+              </h2>
+              {/* output for the request */}
+              <div className="relative w-full rounded-md bg-gray-500">
+                {suggestion.split("\n").map((item) => (
+                  <p className="p-1 ml-4">{item}</p>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
